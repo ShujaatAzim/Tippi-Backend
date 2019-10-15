fbbd5e9f9aa0d46099152f00a9106980bfe9987b
Shujaat Azim <shujaat.azim@gmail.com>
2019-10-14 23:59:17 -0400

    controller methods

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,38 @@
 class TipsController < ApplicationController
+
+    def index 
+        @tips = Tip.all
+        render json: @tips.to_json()
+    end
+
+    def show
+        @tip = Tip.find(params[:id])
+        render json: @tip.to_json()
+    end
+
+    def new
+        @tip = Tip.new
+    end
+
+    def create
+        @tip = Tip.new(tip_params)
+        @tip.save
+    end
+
+    # def update
+    #     @tip = Tip.find(params[:id])
+    # end
+
+
+    def destroy
+        @tip = Tip.find(params[:id])
+        @tip.destroy
+    end
+
+    private
+
+    def tip_params
+        params.require(:item).permit(:amount, :date, :restaurant)
+    end
+
 end
