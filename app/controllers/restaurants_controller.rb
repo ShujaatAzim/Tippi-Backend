dbe409470fd8c7e93e0973b35882173a329e97a0
Shujaat Azim <shujaat.azim@gmail.com>
2020-11-03 04:45:13 -0500

    Changed the indentation in the controllers and fixed some methods.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,40 +1,40 @@
 class RestaurantsController < ApplicationController
 
-    def index
-        @restaurants = Restaurant.all
-        render json: RestaurantSerializer.new(@restaurants).to_serialized_json
-    end
+	def index
+		@restaurants = Restaurant.all
+		render json: RestaurantSerializer.new(@restaurants).to_serialized_json
+	end
 
-    def show
-        @restaurant = Restaurant.find(params[:id])
-        render json: RestaurantSerializer.new(@restaurant).to_serialized_json
-    end
+	def show
+		@restaurant = Restaurant.find(params[:id])
+		render json: RestaurantSerializer.new(@restaurant).to_serialized_json
+	end
 
-    def new
-        @restaurant = Restaurant.new
-    end
+	def new
+		@restaurant = Restaurant.new
+	end
 
-    def create
-        @restaurant = Restaurant.create(name: params[:name], location: params[:location])
-        render json: @restaurant.to_json()
-    end
+	def create
+		@restaurant = Restaurant.create(name: params[:name], location: params[:location])
+		render json: @restaurant.to_json()
+	end
 
-    def update
-        @restaurant = Restaurant.find(params[:id])
-        @restaurant.update_attributes(restaurant_params)
-        render json: @restaurant.to_json()
-    end
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update_attributes(restaurant_params)
+		render json: @restaurant.to_json()
+	end
 
-    def destroy
-        @restaurant = Restaurant.find(params[:id])
-        @restaurant.delete
-    end
+	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.delete
+	end
 
 
-    private
+	private
 
-    def restaurant_params
-        params.require(:restaurant).permit(:name, :location)
-    end
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :location)
+	end
 
 end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,39 +1,39 @@
 class TipsController < ApplicationController
 
-    def index 
-        @tips = Tip.all
-        render json: TipSerializer.new(@tips).to_serialized_json
-    end
+	def index 
+		@tips = Tip.all
+		render json: TipSerializer.new(@tips).to_serialized_json
+	end
 
-    def show
-        @tip = Tip.find(params[:id])
-        render json: TipSerializer.new(@tip).to_serialized_json
-    end
+	def show
+		@tip = Tip.find(params[:id])
+		render json: TipSerializer.new(@tip).to_serialized_json
+	end
 
-    def new
-        @tip = Tip.new
-    end
+	def new
+		@tip = Tip.new
+	end
 
-    def create
-        @tip = Tip.create(tip_params)
-        render json: @tip.to_json()
-    end
+	def create
+		@tip = Tip.create(tip_params)
+		render json: @tip.to_json()
+	end
 
-    def update
-        @tip = Tip.find(params[:id])
-        @tip.update_attributes(tip_params)
-        render json: @tip.to_json()
-    end
+	def update
+		@tip = Tip.find(params[:id])
+		@tip.update_attributes(tip_params)
+		render json: @tip.to_json()
+	end
 
-    def destroy
-        @tip = Tip.find(params[:id])
-        @tip.delete
-    end
+	def destroy
+		@tip = Tip.find(params[:id])
+		@tip.delete
+	end
 
-    private
+	private
 
-    def tip_params
-        params.require(:tip).permit(:amount, :date, :restaurant_id)
-    end
+	def tip_params
+		params.require(:tip).permit(:amount, :date, :restaurant_id)
+	end
 
 end
