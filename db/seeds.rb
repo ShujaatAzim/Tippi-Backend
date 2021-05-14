ea0d3d4809ec30f5be3b55bdb014510c8de3fd80
Shujaat Azim <shujaat.azim@gmail.com>
2021-05-13 20:18:25 -0400

    Changed CORS and seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
+print "Seeding..."
 
-restaurant1 = Restaurant.create(name: "Gringo's")
-restaurant2 = Restaurant.create(name: "Iron Age")
-restaurant3 = Restaurant.create(name: "Clyde's")
-restaurant4 = Restaurant.create(name: "Honey Pig")
-restaurant5 = Restaurant.create(name: "Fogo de Chao")
+restaurant1 = Restaurant.create(name: "Gringo's", location: "Rockville, MD")
+restaurant2 = Restaurant.create(name: "Iron Age", location: "Rockville, MD")
+restaurant3 = Restaurant.create(name: "Clyde's", location: "Rockville, MD")
+restaurant4 = Restaurant.create(name: "Honey Pig", location: "Gaithersburg, MD")
+restaurant5 = Restaurant.create(name: "Fogo de Chao", location: "Tyson's Corner, VA")
 
-tip1 = Tip.create(amount: "$5.00", restaurant_id: 1)
-tip2 = Tip.create(amount: "$10.00", restaurant_id: 2)
-tip3 = Tip.create(amount: "$15.00", restaurant_id: 3)
-tip4 = Tip.create(amount: "$20.00", restaurant_id: 4)
-tip5 = Tip.create(amount: "$25.00", restaurant_id: 5)
+tip1 = Tip.create(amount: "$15.00", restaurant_id: 1, date: "January 13th, 2021")
+tip2 = Tip.create(amount: "$10.00", restaurant_id: 2, date: "February 27th, 2021")
+tip3 = Tip.create(amount: "$15.00", restaurant_id: 3, date: "March 9th, 2021")
+tip4 = Tip.create(amount: "$20.00", restaurant_id: 4, date: "April 19th, 2021")
+tip5 = Tip.create(amount: "$25.00", restaurant_id: 5, date: "May 6th, 2021")
+
+print "Done!"
